735d232ec3aee5069c0b0f18a2957f72e1012399
VBhavya <32004423+VBhavya@users.noreply.github.com>
2017-09-16 07:58:12 +0530

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		Student st1 = new Student(01,"VBS",new Date(1997,05,26),94.650);
+		Student st2 = new Student(02,"VPC",new Date(1992,11,19),83.789);
+		Student st3 = new Student(03,"VSD",new Date(1970,11,02),71.615);
+		Student st4 = new Student(04,"VHK",new Date(1968,09,17),69.801);
+	}
+
+}
